63059bff20e362edbaebe4ff6e2872d2e78236f4
Rahul Bansal <rahul.bansal@rtcamp.com>
2017-08-03 16:52:31 +0530

    psr2

--- stripe-xero.php
+++ stripe-xero.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once( 'vendor/autoload.php' );
+require_once('vendor/autoload.php');
 
 use League\Csv\Writer;
 
 /**
  * Config
  */
-$dotenv = new Dotenv\Dotenv( __DIR__ );
+$dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
 
 //TODO :: move api key to dotenv
@@ -20,17 +20,17 @@ $timezone = getenv('STRIPE_TIMEZONE');
  */
 
 //timezone
-date_default_timezone_set( $timezone  );
+date_default_timezone_set($timezone);
 
 //macOS CSV line ending fix
-if ( ! ini_get( "auto_detect_line_endings" ) ) {
-	ini_set( "auto_detect_line_endings", '1' );
+if (! ini_get("auto_detect_line_endings")) {
+    ini_set("auto_detect_line_endings", '1');
 }
 
 //csv
 $header = [ 'Date', 'Amount', 'Payee', 'Description', 'Reference' ];
-$csv    = Writer::createFromString( '' );
-$csv->insertOne( $header );
+$csv    = Writer::createFromString('');
+$csv->insertOne($header);
 
 
 /**
@@ -40,10 +40,10 @@ echo "Fetching all Stripe customers... \n";
 
 $customer_email = array();
 
-$customers = \Stripe\Customer::all( array( "limit" => 100 ) );
+$customers = \Stripe\Customer::all(array( "limit" => 100 ));
 
-foreach ( $customers->autoPagingIterator() as $customer ) {
-	$customer_email [ $customer->id ] = $customer->email;
+foreach ($customers->autoPagingIterator() as $customer) {
+    $customer_email [ $customer->id ] = $customer->email;
 }
 
 /**
@@ -53,10 +53,10 @@ echo "Fetching all Stripe charges... \n";
 
 $charge_email = array();
 
-$charges = \Stripe\Charge::all( array( "limit" => 100 ) );
+$charges = \Stripe\Charge::all(array( "limit" => 100 ));
 
-foreach ( $charges->autoPagingIterator() as $charge ) {
-	$charge_email[ $charge->id ] = $customer_email[ $charge->customer ];
+foreach ($charges->autoPagingIterator() as $charge) {
+    $charge_email[ $charge->id ] = $customer_email[ $charge->customer ];
 }
 
 /**
@@ -66,10 +66,10 @@ echo "Fetching all Stripe refunds... \n";
 
 $refund_email = array();
 
-$refunds = \Stripe\Refund::all( array( "limit" => 100 ) );
+$refunds = \Stripe\Refund::all(array( "limit" => 100 ));
 
-foreach ( $refunds->autoPagingIterator() as $refund ) {
-	$refund_email [ $refund->id ] = $charge_email[ $refund->charge ];
+foreach ($refunds->autoPagingIterator() as $refund) {
+    $refund_email [ $refund->id ] = $charge_email[ $refund->charge ];
 }
 
 
@@ -79,73 +79,74 @@ $txn_date = new DateTime();
 echo "Fetching all Stripe balance affecting transactions... \n";
 
 //Get Stripe balance affecting transactions
-$transactions = \Stripe\BalanceTransaction::all( array( "limit" => 100 ) );
+$transactions = \Stripe\BalanceTransaction::all(array( "limit" => 100 ));
 
-foreach ( $transactions->autoPagingIterator() as $transaction ) {
-	$row = array();
+foreach ($transactions->autoPagingIterator() as $transaction) {
+    $row = array();
 
-	//csv-date
-	$row[] = ( new DateTime( '@' . $transaction->created ) )->setTimezone( new DateTimeZone( $timezone ) )->format( 'd/m/Y' );
+    //csv-date
+    $tx_date = ( new DateTime('@' . $transaction->created) )->setTimezone(new DateTimeZone($timezone))->format('d/m/Y');
+    $row[] = $tx_date;
 
-	//csv-amount
-	$row[] = $transaction->amount / 100;
+    //csv-amount
+    $row[] = $transaction->amount / 100;
 
-	//csv-Payee
-	switch ( $transaction->type ) {
-		case 'charge':
-			$row[] = $charge_email[ $transaction->source ];
-			break;
-		case 'refund':
-			$row[] = $refund_email[ $transaction->source ];
-			break;
-		case 'payout':
-			$row[] = 'Stripe';
-			break;
-		default :
-			echo "TODO :: unknown transaction of type $transaction->type encountered \n";
-			var_dump( $transaction->__toArray( true ) );
-	}
+    //csv-Payee
+    switch ($transaction->type) {
+        case 'charge':
+            $row[] = $charge_email[ $transaction->source ];
+            break;
+        case 'refund':
+            $row[] = $refund_email[ $transaction->source ];
+            break;
+        case 'payout':
+            $row[] = 'Stripe';
+            break;
+        default:
+            echo "TODO :: unknown transaction of type $transaction->type encountered \n";
+            var_dump($transaction->__toArray(true));
+    }
 
-	//csv-Description
-	$row[] = $transaction->description;
+    //csv-Description
+    $row[] = $transaction->description;
 
-	//csv-reference
-	$row[] = $transaction->source;
+    //csv-reference
+    $row[] = $transaction->source;
 
-	//insert $row into CSV
-	$csv->insertOne( $row );
+    //insert $row into CSV
+    $csv->insertOne($row);
 
-	//Handle non-zero fees
-	if ( $transaction->fee ) {
-		//only if it's stripe fee
-		if ( 'stripe_fee' === $transaction->fee_details[0]['type'] ) {
-			$row = array();
+    //Handle non-zero fees
+    if ($transaction->fee) {
+        //only if it's stripe fee
+        if ('stripe_fee' === $transaction->fee_details[0]['type']) {
+            $row = array();
 
-			//csv-date
-			$row[] = ( new DateTime( '@' . $transaction->created ) )->setTimezone( new DateTimeZone( $timezone ) )->format( 'd/m/Y' );
+            //csv-date
+            $row[] = $tx_date;
 
-			//csv-amount
-			$row[] = $transaction->fee / 100;
+            //csv-amount
+            $row[] = $transaction->fee / 100;
 
-			//csv-Payee
-			$row[] = 'Stripe';
+            //csv-Payee
+            $row[] = 'Stripe';
 
-			//csv-Description
-			$row[] = $transaction->fee_details[0]['description'];
+            //csv-Description
+            $row[] = $transaction->fee_details[0]['description'];
 
-			//csv-reference
-			$row[] = $transaction->source;
+            //csv-reference
+            $row[] = $transaction->source;
 
-			//insert $row into CSV
-			$csv->insertOne( $row );
-		} else {
-			// non stripe fee
-			echo "TODO :: unknown fees of type" . $transaction->fee_details[0]['type'] . "encountered \n";
-			var_dump( $transaction->__toArray( true ) );
-		}
-	}
+            //insert $row into CSV
+            $csv->insertOne($row);
+        } else {
+            // non stripe fee
+            echo "TODO :: unknown fees of type" . $transaction->fee_details[0]['type'] . "encountered \n";
+            var_dump($transaction->__toArray(true));
+        }
+    }
 }
 
 echo $csv; //returns the CSV document as a string
 
-file_put_contents( "stripe-xero-" . ( new DateTime() )->format( 'd-M-Y-h-i-a' ) . ".csv", $csv );
+file_put_contents("stripe-xero-" . ( new DateTime() )->format('d-M-Y-h-i-a') . ".csv", $csv);
